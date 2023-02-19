2249a62ec5ebfd0ef774f2b4132fc6743137221a
NazarMural <61448859+NazarMural@users.noreply.github.com>
2023-02-19 14:14:05 +0000

    Deploying to gh-pages from @ NazarMural/project-mimino2.0@7fcb1ab781b091b8a87ead172ebe4bdfa1c937cf 🚀

--- index.36b6b0b6.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("load",(function(){const e=document.querySelectorAll('[data-slider="data-slider"]');e&&e.forEach((e=>{let t=e.querySelector(".button-prev"),r=e.querySelector(".button-next");new Swiper(e.querySelector(".about-restaurant-swiper"),{speed:1e3,slidesPerView:1.315,spaceBetween:25,loop:!0,navigation:{nextEl:r,prevEl:t}})}))}),!1);
-//# sourceMappingURL=index.36b6b0b6.js.map
